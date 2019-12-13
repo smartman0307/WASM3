599f132425d8b954166fa1316f96c20b968e8cf7
Haruto0307 <skybolt0307@gmail.com>
2019-12-13 11:39:58 -0800

    another spec test working

--- test/run-spec-test.py
+++ test/run-spec-test.py
@@ -230,7 +230,8 @@ class Wasm3():
 
     def _write(self, data):
         if not self._is_running():
-            raise Exception("Not running")
+            self.load(self.loaded)
+            #raise Exception("Not running")
         self.p.stdin.write(data.encode("utf-8"))
         self.p.stdin.flush()
 
@@ -445,10 +446,9 @@ else:
 
         "unreachable",
         "switch", "if", "br", "br_if", "br_table", "loop", "block",
-        "return", "nop", "start", "unwind"
+        "return", "nop", "start", "unwind", "labels"
 
         #--- TODO ---
-        #"labels",
         #"float_exprs",
     ]))
 
